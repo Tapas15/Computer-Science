b83395a7dbb82e26525045279974e27646c3bb5b
Tapas <tapas.mahanandia@gmail.com>
2020-07-20 19:08:23 -0700

    all done

---
+++ Java/Basic/Using Variables/src/using/variables/UsingVariables.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package using.variables;
+
+/**
+ *
+ * @author Home
+ */
+public class UsingVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int myNumber = 120;
+        short myShort = 873;
+        long myLong = 9797;
+        
+        double myDouble = 7.3324;
+        float myfloat = 434.3f;
+        
+        char myChar = 'y';
+        boolean myBool = false;
+        
+        byte myByte = 127;
+        
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myfloat);
+        System.out.println(myBool);
+        System.out.println(myByte);
+        
+    }
+    
+}
