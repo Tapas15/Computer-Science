c938737f9debb02aa4132b8ad84add8969828483
Tapas <tapas.mahanandia@gmail.com>
2020-10-05 12:02:37 +0530

    java files Tutorials added

---
+++ Java/Tutorial02/src/Application.java
@@ -0,0 +1,28 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 9797;
+		
+		double myDouble = 7.3244;
+		float myFloat = 343.3f;
+		
+		char myChar = 'y';
+		boolean myBool = false;
+		
+		byte myBite = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myBite);
+
+	}
+
+}
